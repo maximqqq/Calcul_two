3bddb3d86967e348de03a99660665a83427a5e11
Max <https://github.com/maximqqq>
2017-11-26 14:38:18 +0300

    1

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,18 +3,24 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Calculator extends JFrame{
+public class Calculator extends JFrame {
 
     private JTextField textField1;
     private JTextField textField2;
     private JLabel label;
     private JButton button;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+
+
+
     JButton exitButton;
 
     public Calculator()
     {
         setTitle("КАЛЬКУЛЯТОР");
-        this.setSize(200,300);
+        this.setSize(300,150);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         setLayout(new BorderLayout());
@@ -45,6 +51,7 @@ public class Calculator extends JFrame{
         JPanel panel = new JPanel();
         JPanel panel2 = new JPanel();
         JPanel panel3 = new JPanel();
+        JPanel panel4 = new JPanel();
 
         textField1 = new JTextField(3);
 
@@ -60,18 +67,30 @@ public class Calculator extends JFrame{
 
 ////////////////////////////////////////////////////////////////////////////////
 
+
+
         label = new JLabel();
 
 
 
 
-        button = new JButton("Сложить");
+        button = new JButton("+");
+        button1 = new JButton("-");
+        button2 = new JButton("*");
+        button3 = new JButton("/");
 
         button.addActionListener(new ButtonListoner());
-
+        button1.addActionListener(new ButtonListoner1());
+        button2.addActionListener(new ButtonListoner2());
+        button3.addActionListener(new ButtonListoner3());
 
 
         panel.add(button,BorderLayout.EAST);
+        panel.add(button1,BorderLayout.NORTH);
+        panel.add(button2,BorderLayout.CENTER);
+        panel.add(button3,BorderLayout.SOUTH);
+
+
         panel3.add(label,BorderLayout.SOUTH);
 
         exitButton = new JButton("Exit");
@@ -86,6 +105,7 @@ public class Calculator extends JFrame{
         add(panel,BorderLayout.NORTH);
         add(panel3,BorderLayout.CENTER);
         add(panel2,BorderLayout.SOUTH);
+        add(panel4,BorderLayout.EAST);
 
 
 
@@ -111,14 +131,71 @@ public class Calculator extends JFrame{
                 t2 = Integer.parseInt(textField2.getText());
 
 
-                label.setText("answer:" + (t1 + t2));
+                label.setText("answer :" + (t1 + t2));
                 label.setVisible(true);
 
         }
     }
-//-----------------------------------------------------------------------------
+//-------------------------------------------------------------------------------
+
+//-------СОБЫТИЕ  СЛУШАТЕЛЬ ВЫЧИТАНИЕ--------------------------------------------
+private class ButtonListoner1 implements ActionListener
+{
+
+    @Override
+    public void actionPerformed(ActionEvent ev) {
+
+        int t1, t2;
+
+        t1 = Integer.parseInt(textField1.getText());
+        t2 = Integer.parseInt(textField2.getText());
 
 
+        label.setText("answer :" + (t1 - t2));
+        label.setVisible(true);
 
+    }
+}
+//-------------------------------------------------------------------------------
+
+//-------СОБЫТИЕ  СЛУШАТЕЛЬ УМНОЖЕНИЕ--------------------------------------------
+private class ButtonListoner2 implements ActionListener
+{
+
+    @Override
+    public void actionPerformed(ActionEvent ev) {
+
+        int t1, t2;
+
+        t1 = Integer.parseInt(textField1.getText());
+        t2 = Integer.parseInt(textField2.getText());
+
+
+        label.setText("answer :" + (t1 * t2));
+        label.setVisible(true);
+
+    }
+}
+//-------------------------------------------------------------------------------
+
+//-------СОБЫТИЕ  СЛУШАТЕЛЬ ДЕЛЕНИЕ----------------------------------------------
+private class ButtonListoner3 implements ActionListener
+{
+
+    @Override
+    public void actionPerformed(ActionEvent ev) {
+
+        int t1, t2;
+
+        t1 = Integer.parseInt(textField1.getText());
+        t2 = Integer.parseInt(textField2.getText());
+
+
+        label.setText("answer :" + (t1 / t2));
+        label.setVisible(true);
+
+    }
+}
+//-------------------------------------------------------------------------------
 
 }
